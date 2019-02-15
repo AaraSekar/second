f4b76c867087b217bf4ae5ab377f419bebb22ad3
AaraSekar <47564748+AaraSekar@users.noreply.github.com>
2019-02-15 14:21:04 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,8 +1,11 @@
 n=int(input())
-for i in range (2,n+1):
+flag=0
+for i in range (2,n-1):
 	if n%i==0:
-		print("no")
+		flag=1
 		break
-	else:
-		print("yes")
-		break
+if flag ==0:
+	print("yes")
+else:
+	print("no")
+		
